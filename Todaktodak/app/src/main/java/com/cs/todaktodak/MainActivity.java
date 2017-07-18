d9235412120b32df9218257ccf21fa8e63756224
the1020 <yenn2322@gmail.com>
2017-07-18 17:31:59 +0900

    아이콘 추가

--- Todaktodak/app/src/main/java/com/cs/todaktodak/MainActivity.java
+++ Todaktodak/app/src/main/java/com/cs/todaktodak/MainActivity.java
@@ -9,6 +9,8 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private int[] tabIcons = {
+            R.drawable.ic_map,
+            R.drawable.ic_tel,
+            R.drawable.ic_list
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +46,24 @@ public class MainActivity extends AppCompatActivity {
 
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+        setupTabIcons();
+    }
+
+    private void setupTabIcons(){
+        TextView tabOne = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabOne.setText("지도");
+        tabOne.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_map,0,0);
+        tabLayout.getTabAt(0).setCustomView(tabOne);
+
+        TextView tabTwo = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabTwo.setText("정보");
+        tabTwo.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_tel,0,0);
+        tabLayout.getTabAt(1).setCustomView(tabTwo);
+
+        TextView tabThree = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabThree.setText("THREE");
+        tabThree.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_list,0,0);
+        tabLayout.getTabAt(2).setCustomView(tabThree);
     }
 
     private void setupViewPager(ViewPager viewPager) {
