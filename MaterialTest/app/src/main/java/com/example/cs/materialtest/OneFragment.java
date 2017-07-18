5db628055127cadb5c2ff620f741276277e00fa2
the1020 <yenn2322@gmail.com>
2017-07-18 13:11:10 +0900

    material 디자인 추가

---
+++ MaterialTest/app/src/main/java/com/example/cs/materialtest/OneFragment.java
@@ -0,0 +1,107 @@
+package com.example.cs.materialtest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+/**
+ * Created by CS on 2017-07-17.
+ */
+public class OneFragment extends Fragment implements OnMapReadyCallback {
+
+    static final LatLng SEOUL = new LatLng(37.527089, 127.028480);
+    private GoogleMap googleMap = null;
+    private MapView mapView = null;
+
+    public OneFragment() { }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.fragment_one, container, false);
+
+        mapView = (MapView) layout.findViewById(R.id.map);
+        mapView.getMapAsync(this);
+
+        return layout;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        //액티비티가 처음 생성될 때 실행되는 함수
+        if (mapView != null) {
+            mapView.onCreate(savedInstanceState);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+
+        googleMap = map;
+
+        // 마커
+        googleMap.addMarker(new MarkerOptions().position(SEOUL).title("Seoul"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 17.0f));
+    }
+
+}
---
+++ MaterialTest/app/src/main/java/com/example/cs/materialtest/ThreeFragment.java
@@ -0,0 +1,26 @@
+package com.example.cs.materialtest;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.cs.materialtest.R;
+
+/**
+ * Created by CS on 2017-07-17.
+ */
+public class ThreeFragment extends Fragment{
+    public ThreeFragment(){
+    }
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_three, container, false);
+    }
+}
